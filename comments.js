2ddb74e4a5c22bcf664bb3642d1570ddf9f45da6
venkatesh-selvamCG <venkatesh.selvam@capgemini.com>
2025-01-06 06:30:09 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,33 +1,26 @@
 // create web server
-// 1. include http module
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
-var querystring = require('querystring');
+// create comments
+// read comments
 
-var server = http.createServer(function(req, res) {
-    var uri = url.parse(req.url);
-    if (req.method == 'POST') {
-        var postData = '';
-        req.on('data', function(chunk) {
-            postData += chunk;
-        });
-        req.on('end', function() {
-            var params = querystring.parse(postData);
-            console.log(params);
-            res.writeHead(200, {
-                'Content-Type': 'text/plain'
-            });
-            res.end('You sent the name "' + params.name + '".');
-        });
-    } else {
-        fs.readFile('index.html', function(err, data) {
-            res.writeHead(200, {
-                'Content-Type': 'text/html'
-            });
-            res.end(data);
-        });
-    }
+// create web server
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// create comments
+const comments = [
+    { name: 'John', comment: 'Hello World!' },
+    { name: 'Mary', comment: 'Hello Universe!' }
+];
+
+// read comments
+app.get('/comments', (req, res) => {
+    res.json(comments);
 });
-server.listen(8080);
-console.log('Server is running at http://');
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
+
+// test with curl
+// curl http://localhost:3000/comments
